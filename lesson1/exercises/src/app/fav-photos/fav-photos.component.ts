f623d64f7bbc5b6b76e4fd3598ab8c8e24a1859d
Alex <avmetzger@gmail.com>
2019-12-11 18:59:42 -0600

    did part 1 of exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some of my favorite cats';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = '../../assets/img/2c1c5efc-43f1-4749-9a1e-ab26008044a0.jpg';
+  image3 = '../../assets/img/5ff155d2-baa9-4965-affd-b8f1f63545e1.jpg';
+  
 
   constructor() { }
 
