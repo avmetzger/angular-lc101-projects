d85dfae3b69f04d5c79322c81560a67c23609d39
Alex <avmetzger@gmail.com>
2019-12-16 21:07:09 -0600

    lesson 2 done

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
    selector: 'movie-list',
@@ -11,5 +12,16 @@ export class MovieListComponent implements OnInit {
    constructor() { }
 
    ngOnInit() {
+   }
+   addMovie(newTitle: string) {
+      let errorMsg: string = '';
+      if (this.movies.includes(newTitle)){
+         errorMsg= `${newTitle} is already in your movie list.`;
+      } else if (newTitle === ''){
+         errorMsg=`Please enter a movie title`;
+      } else{
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
    }
 }
